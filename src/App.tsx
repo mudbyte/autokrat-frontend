72c51fb5ce88d07573e29a256f3e4179968b6ba6
Don Kanaille <110260901+realdonkanaille@users.noreply.github.com>
2022-08-02 03:05:32 +0200

    Add nav bar (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,111 @@
 
 import React from 'react';
 import MemberTable from "./MemberTable";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography
+} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+
+const drawerWidth = 240;
 
 function App() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+      <div>
+        <Toolbar/>
+        <Divider/>
+        <List>
+          <ListItem key={'Mitgliederliste'} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <FormatListBulletedIcon/>
+              </ListItemIcon>
+              <ListItemText primary={'Mitgliederliste'}/>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </div>
+  );
+
   return (
-    <MemberTable/>
+      <Box sx={{display: 'flex'}}>
+        <CssBaseline/>
+        <AppBar
+            position="fixed"
+            sx={{
+              width: {sm: `calc(100% - ${drawerWidth}px)`},
+              ml: {sm: `${drawerWidth}px`},
+            }}
+        >
+          <Toolbar>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{mr: 2, display: {sm: 'none'}}}
+            >
+              <MenuIcon/>
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Mitgliederliste
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+            component="nav"
+            sx={{width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
+            aria-label="navigation entries"
+        >
+          <Drawer
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              ModalProps={{keepMounted: true}}
+              sx={{
+                display: {xs: 'block', sm: 'none'},
+                '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+              }}
+          >
+            {drawer}
+          </Drawer>
+          <Drawer
+              variant="permanent"
+              sx={{
+                display: {xs: 'none', sm: 'block'},
+                '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+              }}
+              open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box
+            component="main"
+            sx={{flexGrow: 1, p: 3, width: {sm: `calc(100% - ${drawerWidth}px)`}}}
+        >
+          <Toolbar/>
+          <MemberTable/>
+        </Box>
+      </Box>
   );
 }
 
